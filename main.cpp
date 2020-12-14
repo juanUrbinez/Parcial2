066d23a1d67c2f2a5726c8bca35ff09d674474b1
JuanU <juan.urbinez1@udea.edu.co>
2020-12-13 19:14:50 -0500

    PrimercasoDisparo, calculando velocidades inicales

--- main.cpp
+++ main.cpp
@@ -1,13 +1,16 @@
 #include <iostream>
 #include <math.h>
 #include <cmath>
+#include <cstdlib>
 
 using namespace std;
 
+
+
 struct dO
 {
-    float d;
-    float Ho;
+    float d=600;
+    float Ho=100;
     float Xo=0;
     float Yo=Ho;
     float rd0=0,05*d;
@@ -15,18 +18,61 @@ struct dO
 
 struct dD
 {
-    float d;
-    float Hd;
+    float d=600;
+    float Hd=200;
     float Xd=d;
-    float Yd;
+    float Yd=Hd;
     float rdd=0,025d;
 
 };
 
 
+int PrimercasoDisparo(dO DisparoO,dD DisparoD)
+{
+    srand(time(NULL))
+    V0=rand() %100+1;
+    float x,y;
+    float Vx0,Vy0;
+    float V0;
+    int t=0;
+    int angulo;
+    int V=V0;
+    for (V=V0; ;V0+=5)
+    {
+        for(angulo=0;angulo <90;angulo++)
+        {
+            Vx0*cos(angulo*M_PI/180); //calculando velocidad en x
+            Vy0*sin(angulo*M_PI/180); //calculando velocidad en y
+
+        }
+    }
+}
+
+int SegundoCasoDisparo()
+{
+
+}
+
+int TercerCasoDisparo()
+{
+
+}
+
+int CuartoCasoDisparo()
+{
+
+}
+
+int QuintoCasoDisparo()
+{
+
+}
+
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    dO DisparoO;
+    dD DisparoD;
+
     return 0;
 }
